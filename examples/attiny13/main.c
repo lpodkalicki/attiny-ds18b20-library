b3d9ca9fe93c2b2c7db04feeca24a9fe2f71aeda
Łukasz Podkalicki <lpodkalicki@gmail.com>
2018-08-02 07:47:35 +0200

    Add example for ATtiny13.

---
+++ examples/attiny13/main.c
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2018, Łukasz Marcin Podkalicki <lpodkalicki@gmail.com>
+ *
+ * This is ATtiny13 "Print Temperature" example using attiny-ds18b20-library.
+ * https://github.com/lpodkalicki/attiny-ds18b20-library
+ */
+
+#include <stdint.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include "uart.h"
+#include "ds18b20.h"
+
+#define	DS18B20_PIN	PB0
+
+int
+main(void)
+{
+	uint16_t t;
+
+	/* setup */
+	DS18B20_init(DS18B20_PIN);
+
+	/* loop */
+	while (1) {
+		t = DS18B20_read();
+		uart_putt(t);
+		_delay_ms(1000);
+	}
+}
